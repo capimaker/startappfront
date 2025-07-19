39aff1bc248321f55751115720def4eab668b87f
capimaker <capimaker@gmail.com>
2025-07-19 20:06:04 +0200

    se ha modificado tipografía

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import MentorshipSessionForm from './components/mentorship/MentorshipSessionForm';
