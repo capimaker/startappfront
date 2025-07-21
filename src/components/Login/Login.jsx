5ad8d75019469ac587fc428dc99ecbcef79ebf9e
alejandrogoscu <alejandrogoscu@gmail.com>
2025-07-21 18:20:48 +0200

    Formulario Login OK

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import "./Login.css";
-import { useDispatch } from "react-redux";
-import { Inputpass } from "./Inputpass"; 
+import { useDispatch } from 'react-redux';
+import { Inputpass } from './Inputpass';
 import { Inputemail } from './Inputemail';
 import { login } from '../../features/service/authSlice';
 import { useNavigate } from 'react-router-dom';
+import Background from '../common/Background/Background';
+import { Header } from '../common/Header/Header';
+import './Login.css';
 
 const Login = ({ onLogin }) => {
   const [formData, setFormData] = useState({
-    email: "",
-    password: ""
+    email: '',
+    password: '',
   });
   const { email, password } = formData;
 
@@ -29,38 +31,45 @@ const Login = ({ onLogin }) => {
 
     if (login.fulfilled.match(resultAction)) {
       const userLogged = resultAction.payload.user;
-      onLogin(userLogged);       
-      navigate("/");             
+      if (typeof onLogin === 'function') {
+        onLogin(userLogged);
+      }
+
+      navigate('/');
     } else {
-      console.error("Login fallido:", resultAction);
-      alert("Login fallido. Verifica tus credenciales.");
+      console.error('Login fallido:', resultAction);
+      alert('Login fallido. Verifica tus credenciales.');
     }
   };
 
   return (
-    <div className="login-container">
-      <form className="login-form" onSubmit={onSubmit}>
-        <Inputemail
-          className="login-input"
-          type="email"
-          name="email"
-          value={email}
-          placeholder="Email"
-          onChange={onChange}
-        />
-        <Inputpass
-          className="login-input"
-          type="password"
-          name="password"
-          value={password}
-          placeholder="Password"
-          onChange={onChange}
-        />
-        <button className="login-button" type="submit">
-          Iniciar Sesión
-        </button>
-      </form>
-    </div>
+    <Background>
+      <Header />
+      <div className="login-container">
+        <form className="login-form" onSubmit={onSubmit}>
+          <h1 className="login-title">Inicio de Sesión</h1>
+          <Inputemail
+            className="login-input"
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={onChange}
+          />
+          <Inputpass
+            className="login-input"
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={onChange}
+          />
+          <button className="login-button" type="submit">
+            Iniciar Sesión
+          </button>
+        </form>
+      </div>
+    </Background>
   );
 };
 
