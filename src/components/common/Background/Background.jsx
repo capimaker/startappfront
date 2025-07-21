100c9749d391db5a826b6c0156aeff8566d05ddc
alejandrogoscu <alejandrogoscu@gmail.com>
2025-07-21 11:40:53 +0200

    prueba nuevo diseño layout

--- src/components/common/Background/Background.jsx
+++ src/components/common/Background/Background.jsx
@@ -1,9 +1,8 @@
-import backgroundImage from '../../../assets/background.png';
 import './Background.css';
 
 const Background = ({ children }) => {
   return (
-    <div className="background" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div className="background">
       <main>{children}</main>
     </div>
   );
