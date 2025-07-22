28f04b57c9b2b0d45052c3b6dda78fca26ff2102
capimaker <capimaker@gmail.com>
2025-07-22 18:00:08 +0200

    imagenes incorporadas en mentores

--- src/components/mentors/MentorsCard.jsx
+++ src/components/mentors/MentorsCard.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './MentorsCard.css';
 
 const MentorsCard = ({ mentor, onClick }) => {
-  const { name, email, mentorship_category } = mentor;
+  const { name,img_url, email, mentorship_category } = mentor;
 
   return (
     <div className="mentor-card" onClick={onClick}>
+      <img
+        src={img_url || '/placeholder.png'}
+        alt={name}
+        className="mentors-card-image"
+      />
       <div className="mentor-card-content">
-        <h3>{name}</h3>
-        <p><strong>Categorías:</strong> {mentorship_category?.join(', ')}</p>
+        
+
+       {/* <p><strong>Categorías:</strong> {mentorship_category?.join(', ')}</p> */}
         {email && (
           <button
             onClick={(e) => {
